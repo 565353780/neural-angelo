faed38ed6d3dcaf5ced9f96664c91346942aad61
chLi <565353780@qq.com>
2023-09-28 13:32:56 +0800

    add scene_type as input

--- neural_angelo/Demo/train.py
+++ neural_angelo/Demo/train.py
@@ -2,6 +2,7 @@ from neural_angelo.Method.train import trainNA
 
 def demo():
     data_folder_path = '../colmap-manage/output/3vjia_simple/na/'
+    scene_type = 'indoor'
 
-    trainNA(data_folder_path)
+    trainNA(data_folder_path, scene_type)
     return True

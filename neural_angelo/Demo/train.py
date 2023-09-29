8000316058f08c1a3f505cac9075c5773ad5a08f
chLi <565353780@qq.com>
2023-09-29 11:05:54 +0800

    seems like choice of scene_type doesn't matter

--- neural_angelo/Demo/train.py
+++ neural_angelo/Demo/train.py
@@ -2,7 +2,7 @@ from neural_angelo.Method.train import trainNA
 
 def demo():
     data_folder_path = '../colmap-manage/output/3vjia_simple/na/'
-    scene_type = 'indoor'
+    scene_type = 'outdoor'
 
     trainNA(data_folder_path, scene_type)
     return True
